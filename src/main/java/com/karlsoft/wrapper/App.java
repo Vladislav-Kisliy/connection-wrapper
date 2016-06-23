1cf7d27a0373ffadf89bc7978ede199f7daebaec
Vladislav-Kisliy <vladislav.kisliy@gmail.com>
2016-06-23 22:20:10 +0300

    Added plain proxy, conf application via spring

--- src/main/java/com/karlsoft/wrapper/App.java
+++ src/main/java/com/karlsoft/wrapper/App.java
@@ -1,12 +1,31 @@
 package com.karlsoft.wrapper;
 
+import com.karlsoft.wrapper.api.ApplicationConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
 public class App {
 
+    @Autowired
+    private ApplicationConfig appConfig;
+
     public static void main(String[] args) {
+        new App().startup();
         System.out.println("Hello World!");
+
+    }
+
+    private void startup() {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-application.xml");
+//        Thinker volunteer = (Thinker) ctx.getBean("volunteer");
+//        AppConfig appConfig = (AppConfig) ctx.getBean("appconfig");
+        System.out.println("port =" + appConfig.getServerPort());
     }
 }
---
+++ src/main/java/com/karlsoft/wrapper/proxy/plain/PlainProxy.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2016 vlad
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.karlsoft.wrapper.proxy.plain;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public final class PlainProxy {
+
+    static final int LOCAL_PORT = Integer.parseInt(System.getProperty("localPort", "8443"));
+    
+//    static final String REMOTE_HOST = System.getProperty("remoteHost", "www.google.com");
+//    static final int REMOTE_PORT = Integer.parseInt(System.getProperty("remotePort", "443"));
+    
+    static final String REMOTE_HOST = System.getProperty("remoteHost", "localhost");
+    static final int REMOTE_PORT = Integer.parseInt(System.getProperty("remotePort", "9443"));
+
+    public static void main(String[] args) throws Exception {
+        System.err.println("Proxying *:" + LOCAL_PORT + " to " + REMOTE_HOST + ':' + REMOTE_PORT + " ...");
+
+        // Configure the bootstrap.
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+             .channel(NioServerSocketChannel.class)
+             .handler(new LoggingHandler(LogLevel.INFO))
+             .childHandler(new PlainProxyInitializer(REMOTE_HOST, REMOTE_PORT))
+             .childOption(ChannelOption.AUTO_READ, false)
+             .bind(LOCAL_PORT).sync().channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
+
