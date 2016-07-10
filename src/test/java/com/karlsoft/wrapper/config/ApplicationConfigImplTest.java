6362f4a910f4bc3094a02956e8680de36390052e
Vladislav-Kisliy <vladislav.kisliy@gmail.com>
2016-07-10 16:44:28 +0300

    Added simple test

---
+++ src/test/java/com/karlsoft/wrapper/config/ApplicationConfigImplTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2016 Vladislav Kislyi <vladislav.kisliy@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.karlsoft.wrapper.config;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Vladislav Kislyi <vladislav.kisliy@gmail.com>
+ */
+public class ApplicationConfigImplTest {
+    
+    public ApplicationConfigImplTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of ALL methods, of class ApplicationConfigImpl.
+     */
+    @Test
+    public void testAllServiceEnabled() {
+        ApplicationConfigImpl instance = new ApplicationConfigImpl("true", "true");
+        assertTrue(instance.isPlainServiceEnabled());
+        assertTrue(instance.isSSLServiceEnabled());
+        
+        instance = new ApplicationConfigImpl("true1", "true");
+        assertFalse(instance.isPlainServiceEnabled());
+        assertTrue(instance.isSSLServiceEnabled());
+        
+        instance = new ApplicationConfigImpl("", "");
+        assertFalse(instance.isPlainServiceEnabled());
+        assertFalse(instance.isSSLServiceEnabled());
+        
+        instance = new ApplicationConfigImpl("false", "false");
+        assertFalse(instance.isPlainServiceEnabled());
+        assertFalse(instance.isSSLServiceEnabled());
+    }
+}
